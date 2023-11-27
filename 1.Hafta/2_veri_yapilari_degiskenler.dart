c2f3b7d7fe2af1d7d6444712b39824e549b7ad5d
Adil Sain <adilsain@hotmail.com>
2023-11-27 19:55:16 +0300

    u

---
+++ 1.Hafta/2_veri_yapilari_degiskenler.dart
@@ -0,0 +1,30 @@
+void main(List<String> args) {
+  //strongly typed dildir.
+  //dynamic tipleri de destekler.
+
+  //dynamic veri yapısına sahip diller:
+  //js. python..
+
+  //strongly type
+  // int a=5;
+  // a="adil";
+
+
+  // dynamic c=1;
+  // c="adil";
+
+  //dart veri yapısı
+  //1-numbers types
+  //int
+  //double
+  //2-String types
+  //3-boolean types
+
+  int yas=22;
+  String ad="Adil";
+  DateTime dogumGunu = DateTime(2001,03,12);
+
+  print("Ad : $ad, Yaş: $yas, ${dogumGunu} ");
+
+
+}
