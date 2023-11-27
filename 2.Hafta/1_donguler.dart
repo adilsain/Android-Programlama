ba78638685502d8aee9dcab5885e3c1a5d334f7e
Adil Sain <adilsain@hotmail.com>
2023-11-27 21:21:27 +0300

    u

---
+++ 2.Hafta/1_donguler.dart
@@ -0,0 +1,20 @@
+void main(List<String> args) {
+  //loops => döngüler
+
+  for (var i = 0; i <= 15; i++) {
+    print(i);
+  }
+
+  print("****************************");
+
+  int sayac = 1;
+
+  while (sayac <= 15) {
+    print(sayac);
+    sayac++;
+  }
+
+  //infinite loops => sonsuz döngüler
+  // continue =>alt satırları çalıştırmaz döngü başına döner
+  //break => döngüden tamamen çıkar
+}
